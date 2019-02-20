0390e06d66193d79b90f9aaae346302a5d3c3fac
Ilya Buzunov <46969838+ZTex275@users.noreply.github.com>
2019-02-20 13:44:18 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,39 +1,38 @@
-function GameStart(){
+function GameStart() {
 
-
-    var process = 1;
-    var matrix = [
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-        ["-", "-", "-"]
-      ];
-      
-      while(process>0){
-        if (((matrix[0][0]&&matrix[0][1]&&matrix[0][2])||(matrix[1][0]&&matrix[1][1]&&matrix[1][2])||(matrix[2][0]&&matrix[2][1]&&matrix[2][2])||(matrix[0][0]&&matrix[1][1]&&matrix[2][2])||(matrix[0][2]&&matrix[1][1]&&matrix[2][0]))=='x'){
-          alert("Победа крестиков!"); 
-          process=0;
-          break;}
-
-          if (((matrix[0][0]&&matrix[0][1]&&matrix[0][2])||(matrix[1][0]&&matrix[1][1]&&matrix[1][2])||(matrix[2][0]&&matrix[2][1]&&matrix[2][2])||(matrix[0][0]&&matrix[1][1]&&matrix[2][2])||(matrix[0][2]&&matrix[1][1]&&matrix[2][0]))=='o'){
-            alert("Победа ноликов!");
-            process=0;
-            break;}  
-      for (var i = 2; i < 10; i++) {
-        if (i % 2 == 0) {
-        var j = prompt("Ход крестиков! Введите первую координату:")
-        var k = prompt("Ход крестиков! Введите вторую координату:")
-        matrix[j][k]='х';
-        console.log(matrix[0][0] + " " + matrix[0][1] + " " + matrix[0][2] + "\n" + matrix[1][0] + " " + matrix[1][1] + " " + matrix[1][2] + "\n" + matrix[2][0] + " "+matrix[2][1] + " " + matrix[2][2])
-      }
-     
-      else{
-        var j = prompt("Ход ноликов! Введите первую координату:")
-        var k = prompt("Ход ноликов! Введите вторую координату:")
-        matrix[j][k]='о';
-        console.log(matrix[0][0] + " " + matrix[0][1] + " " + matrix[0][2] + "\n" + matrix[1][0] + " " + matrix[1][1] + " " + matrix[1][2] + "\n" + matrix[2][0] + " "+matrix[2][1] + " " + matrix[2][2])
-        
-      }
-     }
-    }
+  var proccess=1;
+  var matrix = [
+    ["-","-","-"],
+    ["-","-","-"],
+    ["-","-","-"]
+  ];
+  
+  
+  while (proccess>0){
+    for(i=2; i<=10; i++){
+      if (i % 2 == 0){
+        var j = prompt ("Ход крестиков! Введите первую координату: ")
+        var k = prompt ("Ход крестиков! Введите первую координату: ")
+        matrix[j][k] = "x";
+        console.log (matrix[0][0] + " " + matrix[0][1] + " " + matrix[0][2] + "\n" + matrix[1][0] + " " + matrix[1][1] + " " + matrix[1][2] + "\n" + matrix[2][0] + " " + matrix[2][1] + " " + matrix[2][2])
   }
+  else{
+    var j = prompt ("Ход ноликов! Введите первую координату: ")
+    var k = prompt ("Ход ноликов! Введите первую координату: ")
+    matrix[j][k] = "o";
+    console.log (matrix[0][0] + " " + matrix[0][1] + " " + matrix[0][2] + "\n" + matrix[1][0] + " " + matrix[1][1] + " " + matrix[1][2] + "\n" + matrix[2][0] + " " + matrix[2][1] + " " + matrix[2][2])
+    }
+
+    if (((matrix[0][0]=="x")&&(matrix[0][1]=="x")&&(matrix[0][2]=="x"))||((matrix[1][0]=="x")&&(matrix[1][1]=="x")&&(matrix[1][2]=="x"))||((matrix[2][0]=="x")&&(matrix[2][1]=="x")&&(matrix[2][2]=="x"))||((matrix[0][0]=="x")&&(matrix[1][1]=="x")&&(matrix[2][2]=="x"))||((matrix[0][2]=="x")&&(matrix[1][1]=="x")&&(matrix[2][0]=="x"))||((matrix[0][1]=="x")&&(matrix[1][1]=="x")&&(matrix[2][1]=="x"))||((matrix[0][0]=="x")&&(matrix[1][0]=="x")&&(matrix[2][0]=="x"))){
+        alert("Победа крестиков!");
+        proccess=0;
+        break;}
+    if (((matrix[0][0]=="o")&&(matrix[0][1]=="o")&&(matrix[0][2]=="o"))||((matrix[1][0]=="o")&&(matrix[1][1]=="o")&&(matrix[1][2]=="o"))||((matrix[2][0]=="o")&&(matrix[2][1]=="o")&&(matrix[2][2]=="o"))||((matrix[0][0]=="o")&&(matrix[1][1]=="o")&&(matrix[2][2]=="o"))||((matrix[0][2]=="o")&&(matrix[1][1]=="o")&&(matrix[2][0]=="o"))||((matrix[0][1]=="o")&&(matrix[1][1]=="o")&&(matrix[2][1]=="o"))||((matrix[0][0]=="o")&&(matrix[1][0]=="o")&&(matrix[2][0]=="o"))){
+        alert("Победа ноликов!");
+        proccess=0;
+        break;}
+   
+         }
+       }
+     }
   GameStart()
